2b4af7c81f6f183db02989b42abfc3504c6321c3
Jan von der Assen <jan.vda@gmx.ch>
2018-01-05 14:20:58 +0100

    Add tx Hash creation

--- index.js
+++ index.js
@@ -6,19 +6,39 @@ class Bazojs {
   getAccountInfo(publicKey) {
     let that = this;
     return new Promise(function(resolve, reject) {
-      axios.get(that.formatAccountRequest(publicKey)
-    ).then((res)=>{
-      if (!that.responseWasSuccessfull(res.data)) {
-        reject()
-      }
-      resolve(res.data)
-    }).catch(()=>{
-      reject()
-    })
+      axios.get(that.formatAccountRequest(publicKey))
+      .then((res) => {
+        if (!that.responseWasSuccessfull(res.data)) {
+          reject();
+        }
+        resolve(res.data);
+      })
+      .catch((err) => {
+        reject(err);
+      })
+    });
+  }
+  getTransactionHash(header=0, amount, fee, txCount, sender, recipient){
+    let that = this;
+    return new Promise(function(resolve, reject) {
+      console.log(that.formatTxHashRequest(header, amount, fee, txCount, sender, recipient));
+      axios.post(that.formatTxHashRequest(header, amount, fee, txCount, sender, recipient))
+      .then((res) => {
+        resolve(res.data.content[0].detail);
+      })
+      .catch((err) => {
+        reject(err);
+      })
     });
   }
   formatAccountRequest(publicKey) {
     return `${this.formatServerAddress()}account/${publicKey}`;
+  }
+  formatTxHashRequest(header, amount, fee, txCount, sender, recipient) {
+    if (!(amount > 0 && fee > 0 && txCount && sender && recipient)) {
+      this.throwArgumentError();
+    }
+    return `${this.formatServerAddress()}createFundsTx/${header}/${amount}/${fee}/${txCount}/${sender}/${recipient}`;
   }
   formatServerAddress() {
     if (this.serverAddress.slice(-1) !== '/') {
