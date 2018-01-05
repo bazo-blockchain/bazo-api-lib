4e98b050fc08c752bd62c09342e93dd17ce955bf
Jan von der Assen <jan.vda@gmx.ch>
2018-01-05 17:26:32 +0100

    Implement transaction signing and submission.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const axios = require('axios');
+const elliptic = require('elliptic');
+
 class Bazojs {
   constructor(serverAddress) {
     this.serverAddress = serverAddress || 'http://localhost:8001';
@@ -21,7 +23,6 @@ class Bazojs {
   getTransactionHash(header=0, amount, fee, txCount, sender, recipient){
     let that = this;
     return new Promise(function(resolve, reject) {
-      console.log(that.formatTxHashRequest(header, amount, fee, txCount, sender, recipient));
       axios.post(that.formatTxHashRequest(header, amount, fee, txCount, sender, recipient))
       .then((res) => {
         resolve(res.data.content[0].detail);
@@ -30,6 +31,35 @@ class Bazojs {
         reject(err);
       })
     });
+  }
+  sendRawTransaction(txHash, txSignature){
+    let that = this;
+    return new Promise(function(resolve, reject) {
+      axios.post(that.formatTransactionSubmission(txHash, txSignature))
+      .then((res) => {
+        resolve();
+      })
+      .catch((err) => {
+        reject(err);
+      })
+    });
+  }
+  createAndSubmitTransaction(header, amount, fee, sender, recipient, privateKey) {
+    let that = this;
+    that.getAccountInfo(sender)
+    .then((res) => {
+      console.log('Queried the txCount.. :', res.txCnt);
+      that.getTransactionHash(header, amount, fee, res.txCnt, sender, recipient)
+      .then((res) => {
+        console.log('Queried the Hash.. :', res)
+        let signature = that.signHash(res, privateKey)
+        console.log('Computed the signature.. :', signature);
+        that.sendRawTransaction(res, signature)
+        .then((res) => {
+          console.log('Successfully submitted the transaction!');
+        })
+      })
+    })
   }
   formatAccountRequest(publicKey) {
     return `${this.formatServerAddress()}account/${publicKey}`;
@@ -40,10 +70,29 @@ class Bazojs {
     }
     return `${this.formatServerAddress()}createFundsTx/${header}/${amount}/${fee}/${txCount}/${sender}/${recipient}`;
   }
+  formatTransactionSubmission(txHash, signature) {
+    if (!(signature && txHash)) {
+      this.throwArgumentError();
+    }
+    return `${this.formatServerAddress()}sendFundsTx/${txHash}/${signature}`
+  }
   formatServerAddress() {
     if (this.serverAddress.slice(-1) !== '/') {
       return `${this.serverAddress}/`;
     } return this.serverAddress;
+  }
+  signHash(txHash, privateKey) {
+    if (!(txHash && privateKey)) {
+      this.throwArgumentError();
+    }
+    let curve = new elliptic.ec('p256')
+    let key = curve.keyFromPrivate(privateKey);
+    let signature = ''
+    try {
+      signature = key.sign(txHash);
+    } catch (e) {
+    }
+    return signature.r.toJSON() + signature.s.toJSON();
   }
   responseWasSuccessfull(response) {
     if (typeof response === 'object') {
